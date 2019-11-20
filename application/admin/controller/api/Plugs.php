550c1062618743530e699c1e3ea07fed9e54cba8
qie363 <13794480413@163.com>
2019-11-20 21:36:22 +0800

    上传url

--- application/admin/controller/api/Plugs.php
+++ application/admin/controller/api/Plugs.php
@@ -98,7 +98,7 @@ class Plugs extends Controller
             $file = File::instance('qiniu');
             return ['url' => $file->upload(true), 'token' => $file->buildUploadToken(), 'uptype' => $this->getUploadType()];
         } else {
-            return ['url' => 'betdzfdbet.php/api.plugs/upload', 'token' => uniqid('local_upload_'), 'uptype' => $this->getUploadType()];
+            return ['url' => '/bet365bet.php/api.plugs/upload', 'token' => uniqid('local_upload_'), 'uptype' => $this->getUploadType()];
         }
     }
 
