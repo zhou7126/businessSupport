298726433a2e7bcec3151b85b4850c7b4fe1af49
zhou7126 <41935398+zhou7126@users.noreply.github.com>
2019-12-13 20:08:42 +0800

    修改错误页面

--- thinkphp/convention.php
+++ thinkphp/convention.php
@@ -141,7 +141,7 @@ return [
         // 异常页面的模板文件
         'exception_tmpl'         => __DIR__ . '/tpl/think_exception.tpl',
         // 错误显示信息,非调试模式有效
-        'error_message'          => '页面错误！请稍后再试～',
+        'error_message'          => '页面错误！',
         // 显示错误信息
         'show_error_msg'         => false,
         // 异常处理handle类 留空使用 \think\exception\Handle
